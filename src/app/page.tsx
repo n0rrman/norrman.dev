acf4f351dbdfc00a02d0348ec7dda7bfc2b3506d
Norrman <36770294+n0rrman@users.noreply.github.com>
2024-01-27 13:10:05 +0700

    github action test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-blue-100 text-slate-900 dark:bg-slate-900 dark:text-blue-100">
-      <h1 className="text-6xl">Hello, world!</h1>
+      <h1 className="text-6xl">Hello, World!</h1>
     </div>
   );
 }
